1bf8c0539a91c61b0beec45fc55a045d652b3091
Zsolt_Sebestyen <zsolt.sebestyen@fornax.hu>
2022-10-09 12:28:51 +0200

    teszt

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        BufferedWriter w = new BufferedWriter(new FileWriter("./doc/secret.txt"));
+        w.write("teststtttttttttttttt");
+        w.close();
+
         System.out.println("Hello world!");
+
+
     }
 }
