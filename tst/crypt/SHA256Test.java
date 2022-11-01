8a407f0fb5e9c470d523462f39e030a44600141e
Zsolt_Sebestyen <zsolt.sebestyen@fornax.hu>
2022-11-01 21:14:58 +0100

    fix warnings

--- tst/crypt/SHA256Test.java
+++ tst/crypt/SHA256Test.java
@@ -1,18 +1,10 @@
 package crypt;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SHA256Test {
-
-    SHA256 sha256;
-
-    @BeforeEach
-    void setUp() {
-        sha256 = new SHA256();
-    }
 
     @Test
     void encrypt() throws CryptoException {
