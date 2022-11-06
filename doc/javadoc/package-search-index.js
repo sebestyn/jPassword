62696bc215b68dd71b61eee3d0063360a03146a0
Zsolt Sebestyén <sebestyn.zsolt@gmail.com>
2022-11-06 19:39:43 +0100

    javadoc

---
+++ doc/javadoc/package-search-index.js
@@ -0,0 +1,5 @@
+packageSearchIndex = [{
+    "l": "All Packages",
+    "u": "allpackages-index.html"
+}, {"l": "crypt"}, {"l": "dataTypes"}, {"l": "gui"}, {"l": "gui.Controller"}, {"l": "gui.View"}];
+updateSearchResults();
